cd5fe0238b5f34868f544f57f0b9437c2ad296d1
Darkcdm <darksniped@gmail.com>
2022-09-21 14:25:43 +0200

    fixed onload; Routed Code via Main; Created Pawn Class; Created Pairing Object;

---
+++ Main.js
@@ -0,0 +1,7 @@
+let Main = {
+	body: [],
+	start: function (body) {
+		//this.body = body;
+		GameBoard.render(body);
+	},
+};
---
+++ Pawn.js
@@ -0,0 +1,9 @@
+class Pawn {
+	constructor(startX, startY, Colour) {
+		this.X = startX;
+		this.Y = startY;
+		this.Colour = Colour;
+	}
+	checkLegalMove(desiredX, desiredY) {}
+	move() {}
+}
