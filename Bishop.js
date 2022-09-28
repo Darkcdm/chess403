3fcdfa48b40485d5c265797d2beae9d2cbdef8fa
Daniel Ivanco <danikopava1@gmail.com>
2022-09-28 18:05:30 +0200

    created class bishop; created class knight; created class king; created class queen; modified Gamestart.js with new classes; added new script in index;

---
+++ Bishop.js
@@ -0,0 +1,11 @@
+class Bishop {
+	constructor(startX, startY, Colour) {
+		this.X = startX;
+		this.Y = startY;
+		this.Colour = Colour;
+
+		this.Symbol = "B";
+	}
+	checkLegalMove(desiredX, desiredY) {}
+	move() {}
+}
--- GameStart.js
+++ GameStart.js
@@ -1,6 +1,12 @@
 let GameStart = {
 	setBlacks: function () {
 		this.setPawns(2, "Black");
+		this.setBishop(1, 3, "Black");
+		this.setBishop(1, 6, "Black");
+		this.setKnight(1, 2, "Black");
+		this.setKnight(1, 7, "Black");
+		this.setQueen(1, 4, "Black");
+		this.setKing(1, 5, "Black");
 	},
 	setPawns: function (row, colour) {
 		y = row;
@@ -11,4 +17,54 @@ let GameStart = {
 			console.log(pawn);
 		}
 	},
+	setBishop: function (row, collum, colour) {
+		y = row;
+		x = collum;
+
+		if (y == 1 && x == 3){
+			bishop = new Bishop(x, y, colour);
+			Main.BlackPieces.push(bishop);
+			console.log(bishop);
+		}
+		else if (y == 1 && x == 6){
+			bishop = new Bishop(x, y, colour);
+			Main.BlackPieces.push(bishop);
+			console.log(bishop);
+		}
+	},
+	setKnight: function (row, collum, colour) {
+		y = row;
+		x = collum;
+	
+		if (y == 1 && x == 2){
+			knight = new Knight(x, y, colour);
+			Main.BlackPieces.push(knight);
+			console.log(knight);
+		}
+		else if (y == 1 && x == 7){
+			knight = new Knight(x, y, colour);
+			Main.BlackPieces.push(knight);
+			console.log(knight);
+		}
+	},
+	setQueen: function (row, collum, colour) {
+		y = row;
+		x = collum;
+	
+		if (y == 1 && x == 4){
+			queen = new Queen(x, y, colour);
+			Main.BlackPieces.push(queen);
+			console.log(queen);
+		}
+	},
+	setKing: function (row, collum, colour) {
+		y = row;
+		x = collum;
+	
+		if (y == 1 && x == 1){
+			king = new King(x, y, colour);
+			Main.BlackPieces.push(king);
+			console.log(king);
+		}
+	}
 };
